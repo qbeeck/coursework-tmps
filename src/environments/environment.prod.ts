e7ae4375f8843005956e2baa475998efa09cac3e
Artiom Olari <artiom@bend.md>
2023-05-21 15:24:03 +0300

    delete unused code and create singleton pattern

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
-export const environment = {
+import { EnvironmentI } from '@core';
+
+export const environment: EnvironmentI = {
   production: true
 };
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { Environment } from '@core';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+/**
+ * create environment singleton instance on application boostraping
+ * and set environment
+ */
+const envInstance = Environment.getInstance();
+envInstance.setEnvironment(environment);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
